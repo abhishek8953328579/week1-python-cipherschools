d86ccdc3903349e579d23d6a41bff01b4811a47e
abhishek8953328579 <119608522+abhishek8953328579@users.noreply.github.com>
2022-12-01 22:11:47 +0530

    Create solv exc.py

---
+++ solv exc.py
@@ -0,0 +1,8 @@
+
+name = "         Har        shit       "
+dots = "............................... "
+print(name + dots)
+print(name.lstrip() + dots)
+print(name.rstrip() + dots)
+print(name.strip() + dots)
+print(name.replace(" ", "") + dots)
